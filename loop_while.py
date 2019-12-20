1de4b13e3a1551ec5a17917e3421fe068bcae718
Amit Bhatia <amitbhatia76@gmail.com>
2019-12-20 15:00:26 -0500

    More code.

---
+++ loop_while.py
@@ -0,0 +1,12 @@
+
+# Examle of while loop # Create a variable name x and set the variable to zero.
+x=0
+
+# Use a loop e.g. For, While to print first 10 integers
+while x is not 10:
+    x=x+1
+    print(x)
+    
+# Print Done in the end
+print('done!')
+ 
