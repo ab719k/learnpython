90fe76ec8fb7b82795da8c0aac34f852d076b601
Amit Bhatia <amitbhatia76@gmail.com>
2019-12-20 14:38:32 -0500

    Update sum_two_numbers.py

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -13,11 +13,13 @@
 # Variables are containers for storing data values
 # A variable is created the moment you first assign a value to it.
 
-num1=int(input("Enter First Number"))
-num2=int(input("Enter Second Number"))
+x=int(input("Enter First Number : "))
+y=int(input("Enter Second Number : "))
 
+#Add the two numbers and store in third variable
+s = x + y
 
-print("Sum of two numbers is " + (num1+num2))
+print("Sum of two numbers is %d " % (s))
 
 
          
