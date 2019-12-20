5202e6be5d5477c7a65c28ec2670ed0e31e3918d
Amit Bhatia <amitbhatia76@gmail.com>
2019-12-20 14:33:31 -0500

    Update sum_two_numbers.py

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,1 +1,23 @@
 
+
+# Syntax: Syntax is essentially the ‘spelling and grammar’ of computer programming languages.
+# Just as it may be difficult to understand an English sentence without proper spelling and grammar,
+# a computer can’t understand their commands unless they are laid out properly.
+# Syntax defines the proper way to lay out commands in programming languages.
+
+# Pound(#) indicate the start of comment.
+# It tells the python interpretor that it should not execute this line i.e. take no action.
+
+# First Step: Ask user to input two variable one at a time.
+# A variable is a type of value that can change.
+# Variables are containers for storing data values
+# A variable is created the moment you first assign a value to it.
+
+num1=int(input("Enter First Number"))
+num2=int(input("Enter Second Number"))
+
+
+print("Sum of two numbers is " + (num1+num2))
+
+
+         
