03f38211c4e2555f242e2c31c7f308747640ccd4
Amit Bhatia <amitbhatia76@gmail.com>
2020-01-04 10:48:46 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello World")
+#!/usr/local/bin/python3
+# This program says hello and asks for your name.
+print('Hello world!')
+myName = input('What is your name? ')
+print('It is good to meet you, ' + myName + '!')
